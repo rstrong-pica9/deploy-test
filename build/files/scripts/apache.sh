b934cf2a6c4e1a85f080b23ed1207f59210306b0
Rob Strong <robert.a.strong@gmail.com>
2014-01-27 00:06:32 -0500

    removing some ssl functionality

--- build/files/scripts/apache.sh
+++ build/files/scripts/apache.sh
@@ -4,5 +4,4 @@ set -e
 yum -y --enablerepo=remi install httpd mod_ssl
 mkdir -p /etc/httpd/conf
 cp /root/docker/httpd.conf /etc/httpd/conf/httpd.conf
-cp -R /root/docker/ssl /etc/httpd/
 useradd www-data
