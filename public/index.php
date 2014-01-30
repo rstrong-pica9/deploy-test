97edd54c6296b809de21869356b6865a4a229566
Rob Strong <rstrong@pica9.com>
2014-01-30 14:11:36 -0500

    testing redis

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
+<?php 
+
+require_once "../vendor/autoload.php";
+
+$client = new Predis\Client(getenv('REDIS_PORT_6379_TCP'));
+$client->set('foo', 'bar');
+
+?>
 Deployment Works!<br>
-IP: <?= $_SERVER['SERVER_ADDR'] ?>
+IP: <?= $_SERVER['SERVER_ADDR'] ?><br>
+Redis: <?= getenv('REDIS_PORT_6379_TCP') ?><br>
+Redis Test:<br>
+&nbsp;&nbsp;&nbsp;&nbsp;These should be equal: bar = <?= $client->get('foo'); ?><br>
 <?php phpinfo();
