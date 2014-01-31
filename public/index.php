3c36bce1c0faa0662213710c3182306bbe6f43ef
Rob Strong <rstrong@pica9.com>
2014-01-31 08:54:50 -0500

    testing postgres addon

--- public/index.php
+++ public/index.php
@@ -2,8 +2,20 @@
 
 require_once "../vendor/autoload.php";
 
+//redis test
 $client = new Predis\Client(getenv('REDIS_PORT_6379_TCP'));
 $client->set('foo', 'true');
+
+//postgres test
+try {
+    $pdo = new PDO(
+        'pgsql:dbname=docker;host=' .
+        getenv('DB_PORT_6379_TCP_ADDR') . ';user=docker;password=docker;port=5432;'
+    );
+
+} catch (Exception $e) {
+    echo "Could not connect to postgresql<br>";
+}
 
 ?>
 Deployment Works!<br>
