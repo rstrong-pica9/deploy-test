65303613d90107e66ae7c4530e97b6ca39907c61
Rob Strong <rstrong@pica9.com>
2014-01-31 09:09:57 -0500

    updating postgres test

--- public/index.php
+++ public/index.php
@@ -13,6 +13,22 @@ try {
         getenv('DB_PORT_6379_TCP_ADDR') . ';user=docker;password=docker;port=5432;'
     );
 
+    //try to create testing DB
+    try {
+        $testing = new PDO(
+            'pgsql:dbname=testing;host=' .
+            getenv('DB_PORT_6379_TCP_ADDR') . ';user=docker;password=docker;port=5432;'
+        );
+    } catch (Exception $e) {
+        //DB doesn't exist, create it
+        $pdo->query('CREATE DATABASE testing OWNER=docker;');
+        $testing = new PDO(
+            'pgsql:dbname=testing;host=' .
+            getenv('DB_PORT_6379_TCP_ADDR') . ';user=docker;password=docker;port=5432;'
+        );
+        $testing->query('CREATE TABLE access_log (id serial NOT NULL, created_at timestamp without time zone NOT NULL)');
+    }
+
 } catch (Exception $e) {
     echo "Could not connect to postgresql<br>";
 }
@@ -23,4 +39,10 @@ IP: <?= $_SERVER['SERVER_ADDR'] ?><br>
 <br>
 Redis: <?= getenv('REDIS_PORT_6379_TCP') ?><br>
 Redis Test: true = <?= $client->get('foo'); ?><br>
+<br>
+PostgreSQL Test:<br>
+&nbsp;&nbsp;Access Log Entries
+<?php foreach ($testing->query('SELECT * FROM access_log') as $row) { ?>
+    &nbsp;&nbsp;<?= $row['created_at']; ?><br>
+<?php } ?>
 <?php phpinfo();
