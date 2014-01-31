cc2e4f42bd9a8158ca294d78aebf7b44598bfae9
Rob Strong <rstrong@pica9.com>
2014-01-31 09:46:42 -0500

    fixed db connection

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 require_once "../vendor/autoload.php";
 
 //redis test
@@ -9,30 +8,18 @@ $client->set('foo', 'true');
 //postgres test
 try {
     $pdo = new PDO(
-        'pgsql:dbname=docker;host=' .
-        getenv('DB_PORT_6379_TCP_ADDR') . ';user=docker;password=docker;port=5432;'
+        'pgsql:dbname=docker;host=' . getenv('DB_PORT_5432_TCP_ADDR') . ';port=5432;',
+        'docker',
+        'docker'
     );
 
-    //try to create testing DB
-    try {
-        $testing = new PDO(
-            'pgsql:dbname=testing;host=' .
-            getenv('DB_PORT_6379_TCP_ADDR') . ';user=docker;password=docker;port=5432;'
-        );
-    } catch (Exception $e) {
-        //DB doesn't exist, create it
-        $pdo->query('CREATE DATABASE testing OWNER=docker;');
-        $testing = new PDO(
-            'pgsql:dbname=testing;host=' .
-            getenv('DB_PORT_6379_TCP_ADDR') . ';user=docker;password=docker;port=5432;'
-        );
-        $testing->query('CREATE TABLE access_log (id serial NOT NULL, created_at timestamp without time zone NOT NULL)');
-    }
+        $pdo->query('CREATE TABLE access_log (id serial NOT NULL, created_at timestamp without time zone NOT NULL)');
 
 } catch (Exception $e) {
-    echo "Could not connect to postgresql<br>";
+    echo "Could not connect to postgresql<br>" . $e->getMessage() . "<br>";
 }
 
+$pdo->query("INSERT INTO access_log (created_at) VALUES ('" . date('r') . "')");
 ?>
 Deployment Works!<br>
 IP: <?= $_SERVER['SERVER_ADDR'] ?><br>
@@ -41,8 +28,11 @@ Redis: <?= getenv('REDIS_PORT_6379_TCP') ?><br>
 Redis Test: true = <?= $client->get('foo'); ?><br>
 <br>
 PostgreSQL Test:<br>
-&nbsp;&nbsp;Access Log Entries
-<?php foreach ($testing->query('SELECT * FROM access_log') as $row) { ?>
+&nbsp;&nbsp;Access Log Entries<br>
+<?php $sth = $pdo->query('SELECT * FROM access_log');
+$sth->setFetchMode(PDO::FETCH_ASSOC);
+foreach ($sth->fetchAll() as $row) { ?>
     &nbsp;&nbsp;<?= $row['created_at']; ?><br>
-<?php } ?>
-<?php phpinfo();
+    <?php
+}
+phpinfo();
